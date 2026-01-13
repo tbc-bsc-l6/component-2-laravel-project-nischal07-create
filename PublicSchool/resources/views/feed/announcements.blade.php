9f363060a33ca56342dc690227fa4a4be5db1f77
bsaladk <115446445+bsaladk@users.noreply.github.com>
2026-01-13 19:51:50 +0545

    Fix: use single-line @php for XML declaration

--- PublicSchool/resources/views/feed/announcements.blade.php
+++ PublicSchool/resources/views/feed/announcements.blade.php
@@ -1,7 +1,4 @@
-@php
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-@endphp
-
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <rss version="2.0">
     <channel>
         <title>PublicSchool Announcements</title>
